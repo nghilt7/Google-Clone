03ad3c2fb5461b94e13995772c2ef47578f5d343
nghilt7 <nghilt2307@gmail.com>
2022-06-18 19:22:18 +0700

    create search header section

--- components/User.js
+++ components/User.js
@@ -1,6 +1,6 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function User() {
+export default function User({ className }) {
   const { data: session } = useSession();
   if (session) {
     return (
@@ -12,7 +12,7 @@ export default function User() {
   return (
     <>
       <button
-        className="bg-blue-500 px-6 py-2 font-medium rounded-md text-white hover:brightness-105 hover:shadow-md"
+        className={`bg-blue-500 px-6 py-2 font-medium rounded-md text-white hover:brightness-105 hover:shadow-md ${className}`}
         onClick={signIn}
       >
         Sign In
