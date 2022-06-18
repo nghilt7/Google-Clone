56d99b5c727ea54e281c50785198f2f9c5ae6b41
nghilt7 <nghilt2307@gmail.com>
2022-06-18 15:00:50 +0700

    create footer section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
+import Footer from "./../components/Footer";
 
 export default function Home() {
   return (
@@ -38,6 +39,8 @@ export default function Home() {
       </form>
 
       {/* Body */}
+
+      <Footer />
 
       {/* Footer */}
     </div>
