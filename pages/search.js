daffd0410fc8f13bcf0bb9d87d1a4bb5589bb957
nghilt7 <nghilt2307@gmail.com>
2022-06-19 13:48:35 +0700

    create pagination

--- pages/search.js
+++ pages/search.js
@@ -25,7 +25,8 @@ export default function search({ results }) {
 }
 
 export async function getServerSideProps(context) {
-  const mockData = true;
+  const startIndex = context.query.start || "1";
+  const mockData = false;
   const data = mockData
     ? Response
     : await fetch(
@@ -33,7 +34,7 @@ export async function getServerSideProps(context) {
           process.env.API_KEY
         }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
           context.query.searchType && "&searchType=image"
-        }`
+        }&start=${startIndex}`
       ).then((response) => response.json());
   return {
     props: {
